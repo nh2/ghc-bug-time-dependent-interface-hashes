88fd7e524b2c3e0d8a8af83e0ee45dfafaf5f8d4
Niklas Hambüchen <mail@nh2.me>
2013-08-20 11:26:39 +0900

    Add run-multiple-times.sh

---
+++ run-multiple-times.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# You should see how the interface hash is different between two runs
+# whenever a *new second* has begun (look at the diff times in the output).
+
+./test.sh
+echo
+./test.sh
+echo
+./test.sh
+echo
+./test.sh
+echo
+./test.sh
+echo
+./test.sh
+echo
+./test.sh
+echo
+./test.sh
+echo
+./test.sh
+echo
+./test.sh
+echo
+./test.sh
+echo
+./test.sh
+echo
+./test.sh
+echo
